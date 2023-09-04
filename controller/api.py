24a5d02ffb3c5d9185bddad745ff44c89ab60c7e
Phiwa Dludlu <phiwadludlu79@gmail.com>
2023-09-04 13:56:41 +0200

    Added vue js to clinic page

--- controller/api.py
+++ controller/api.py
@@ -2,10 +2,12 @@ import services.user_service
 import services.user_service as user_service
 from flask import jsonify,request
 from models.user_model import UserSchemaIn, UserSchemaOut
+from models.clinic_model import ClinicIn, Clinic
+from models import db 
 
 user_schema_in = UserSchemaIn()
 user_schema_out = UserSchemaOut()
-
+clinic_schema_in = ClinicIn()
 def show():
     users=user_service.get_users()
     user_list = []
@@ -39,3 +41,20 @@ def create():
         return 'Success',200
     else:
         return "An error has occured",400
+
+def create_clinic():
+    clinic_name = request.json['clinic_name']
+    clinic = Clinic(clinic_name=clinic_name)
+    errors = clinic_schema_in.dump(clinic)
+    if errors:
+        return "Error", 401
+    
+    try:
+        db.session.add(clinic)
+        db.session.commit()
+
+        return "Done",201
+    
+    except:
+
+        return "Error",401
